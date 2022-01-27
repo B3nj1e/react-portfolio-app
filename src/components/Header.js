6cdd635f42355ad7fc8fdd67e9cb952956727762
Benjamin Clewer <benjiclewer@gmail.com>
2022-01-27 13:55:57 +1030

    functioning with react

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,18 @@ import '../styles/Header.css';
 
 // By importing the Header.css file, it is added to the DOM whenever this component loads
 
-// We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
-// Unlike regular HTML, a JSX style property must be an object instead of a string
-// On a style object, we camelCase all property names, and put all of the values in quotes
-// Non quoted values default to "pixels", e.g. height, margin, padding
 
 // We use JSX curly braces to evaluate the style object
 
 function Header() {
   return (
     <header>
-        
-    <div class="page-title">
+    <div className="page-title">
         <h1>Benji Clewer</h1>
         <p>Web Developer</p>
     </div>
-    
-    <div class="navbar">
+
+    <div className="navbar">
         <a href="#about-me" class="nav-button">About</a>
         <a href="#my-work" class="nav-button">Work</a>
         <a href="#contact-me" class="nav-button">Contact</a>
--- src/components/Project.js
+++ src/components/Project.js
@@ -7,12 +7,6 @@ import Picture4  from '../assets/images/Picture4.png'
 import Picture2  from '../assets/images/Picture2.png'
 import Picture123  from '../assets/images/Picture123.png'
 
-// By importing the Section.css file, it is added to the DOM whenever this component loads
-
-// We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
-// Unlike regular HTML, a JSX style property must be an object instead of a string
-// On a style object, we camelCase all property names, and put all of the values in quotes
-// Non quoted values default to "pixels", e.g. height, margin, padding
 
 // We use JSX curly braces to evaluate the style object on the JSX tag
 
